38e11b265c0d333c00c71c05e5373cfe5039f5b8
rbalajee <rahrkb4@gmail.com>
2025-03-13 21:58:55 +0530

    added rds

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -12,3 +12,12 @@ variable "contact" {
   description = "Contact email for tagging resources"
   default     = "rahrkb4@gmail.com"
 }
+
+variable "db_username" {
+  description = "username for the recipe app api db"
+  default     = "recipeapp"
+}
+
+variable "db_password" {
+  description = "password for terraform db"
+}
