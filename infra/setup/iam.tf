88873a0c1481d164187b97dd80dc1148b15c2c0c
rbalajee <rahrkb4@gmail.com>
2025-03-13 22:53:08 +0530

    added-new-policy

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -167,6 +167,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
       "rds:ModifyDBInstance",
+      "rds:AddTagsToResource",
       "rds:AddTagsToResource"
     ]
     resources = ["*"]
