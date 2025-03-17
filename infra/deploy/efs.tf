76d31dd6da543cafb8e678e941ddf37aad0486a6
rbalajee <rahrkb4@gmail.com>
2025-03-17 16:21:08 +0530

    added access point

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -25,17 +25,25 @@ resource "aws_security_group" "efs" {
 }
 
 resource "aws_efs_mount_target" "media_a" {
-  file_system_id = aws_efs_file_system.media.id
-  subnet_id      = aws_subnet.private_a.id
-  security_groups = [
-    aws_security_group.efs.id
-  ]
+  file_system_id  = aws_efs_file_system.media.id
+  subnet_id       = aws_subnet.private_a.id
+  security_groups = [aws_security_group.efs.id]
 }
 
 resource "aws_efs_mount_target" "media_b" {
+  file_system_id  = aws_efs_file_system.media.id
+  subnet_id       = aws_subnet.private_b.id
+  security_groups = [aws_security_group.efs.id]
+}
+
+resource "aws_efs_access_point" "media" {
   file_system_id = aws_efs_file_system.media.id
-  subnet_id      = aws_subnet.private_b.id
-  security_groups = [
-    aws_security_group.efs.id
-  ]
+  root_directory {
+    path = "/api/media"
+    creation_info {
+      owner_gid   = 101
+      owner_uid   = 101
+      permissions = "755"
+    }
+  }
 }
